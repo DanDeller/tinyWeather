45955125113415044365c51af60a4cad68592ba9
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2020-03-14 20:31:34 -0400

    Revert back to React.Component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,19 +2,21 @@ import React from 'react';
 import style from '../styles/style.less';
 import ReactDOM from 'react-dom';
 
-const Sidebar = (city) => {
-  const getRecentCities = city.recentCities.map((city, i) => (
-    <li key={i}>{city}</li>
-  ));    
+class Sidebar extends React.Component {
+  render() {
+    const getRecentCities = this.props.recentCities.map((city, i) => (
+      <li key={i}>{city}</li>
+    ));    
 
-  return (
-    <aside className={style.sideBar}>
-      <h3>Recently Searched Cities:</h3>
-      <ul>
-        {getRecentCities}
-      </ul>
-    </aside>
-  );
+    return (
+      <aside className={style.sideBar}>
+        <h3>Recently Searched Cities:</h3>
+        <ul>
+          {getRecentCities}
+        </ul>
+      </aside>
+    );
+  }
 }
 
 export default Sidebar;
--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -3,22 +3,23 @@ import style from '../styles/style.less';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 
-const WeatherList = (newCity) => {
-  console.log(newCity);
-  const classes = !newCity.isOpen ? style.active : '';
-  const city = newCity.cityDetails.map((item, i) => (
-    <div className={style.weatherItem} key={i}>
-      <h3>City: {item.name}</h3>
-      <p>Current weather: {item.weather}</p>
-      <p>Current temperature: {item.temp}</p>
-    </div>
-  ));
+class WeatherList extends React.Component {
+  render() {
+    const classes = !this.props.isOpen ? style.active : '';
+    const city = this.props.cityDetails.map((item, i) => (
+      <div className={style.weatherItem} key={i}>
+        <h3>City: {item.name}</h3>
+        <p>Current weather: {item.weather}</p>
+        <p>Current temperature: {item.temp}</p>
+      </div>
+    ));
 
-  return (
-    <div className={style.weatherItems + ' ' + classes}>
-      {city}
-    </div>
-  );
+    return (
+      <div className={style.weatherItems + ' ' + classes}>
+        {city}
+      </div>
+    );
+  }
 }
 
 export default WeatherList;
