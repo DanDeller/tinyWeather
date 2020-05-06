f439798c25f9fd22772bddcb723aa6d2abbc0b95
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2020-05-06 15:05:12 -0400

    animation test

--- src/components/SidebarItem.js
+++ src/components/SidebarItem.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import style from '../assets/styles/style.less';
 import PropTypes from 'prop-types';
 
 const SidebarItem = (props) => {
@@ -14,7 +15,12 @@ const SidebarItem = (props) => {
 
   const city = props.city;
   return (
-    <li className={recentCity.completed ? 'active' : 'unactive'} onClick={() => toggle()}>{city.recentCity}</li>
+    <li
+      style={{
+        transform: !recentCity.completed ? 'translateY(0)' : 'translateX(-5vh)',
+        opacity: !recentCity.completed ? '1' : '.4'
+      }}
+      className={`${style.sidebarItem} ${recentCity.completed ? 'sidebarActive' : 'sidebarUnactive'}`} onClick={() => toggle()}>{city.recentCity}</li>
   );
 }
 
