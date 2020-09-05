e576a0e18916b7c385ea77c2ba685bcaa353e7b2
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2020-09-04 21:42:05 -0400

    update logger messages

--- server/models/currentWeatherModel/currentWeather.js
+++ server/models/currentWeatherModel/currentWeather.js
@@ -1,45 +1,46 @@
 const config = require('../../../server.config.js'),
-      logger = require('../../../middleware/logger.js'),
-      r = require('rethinkdb');
+			logger = require('../../../middleware/logger.js'),
+			r = require('rethinkdb');
 
 module.exports = {
-  connect: function(cb) {
-    r.connect({
-      db: config.db.name,
-      host: config.db.host,
-      port: config.db.port
-    })
-    .then((conn) => {
-      return cb(null, conn);
-    })
-    .error((err) => {
-      return cb(err);
-    })
-  },
+	connect: function(cb) {
+		r.connect({
+			db: config.db.name,
+			host: config.db.host,
+			port: config.db.port
+		})
+		.then((conn) => {
+			return cb(null, conn);
+		})
+		.error((err) => {
+			logger.error(`Could not connetct to ${config.db.name}`);
+			return cb(err);
+		})
+	},
 
-  list: function(cb) {
-    this.connect((err, conn) => {
-      if (err) return cb(err);
-      r.db(config.db.name).table('currentWeather')
-      .run(conn)
-      .then((res) => {
-        return res.toArray();
-      })
-      .then((data) => {
-        return cb(null, data);
-      })
-      .error((err) => {
-        logger.error(`Could not find and ${config.db.name} items.`);
-        return cb(err);
-      });
-    })
-  },
+	list: function(cb) {
+		this.connect((err, conn) => {
+			if (err) return cb(err);
+			r.db(config.db.name).table('currentWeather')
+			.run(conn)
+			.then((res) => {
+				return res.toArray();
+			})
+			.then((data) => {
+				return cb(null, data);
+			})
+			.error((err) => {
+				logger.error(`Could not find any ${config.db.tables.currentWeather} items.`);
+				return cb(err);
+			});
+		})
+	},
 
-  post: function(request, cb) {
-    const city = request.body;
+	post: function(request, cb) {
+		const city = request.body;
 		this.connect((err, connection) => {
 			if (err) return cb(err);
-      r.db(config.db.name).table('currentWeather')
+			r.db(config.db.name).table('currentWeather')
 			.insert({
 				id: city.id,
 				city: city.city
@@ -49,17 +50,17 @@ module.exports = {
 				return cb(null, response);
 			})
 			.error((error) => {
-        logger.error(`Error trying to send data to ${config.db.name}.`);
+				logger.error(`Error trying to send data to the ${config.db.tables.currentWeather} table.`);
 				return cb(error);
 			});
 		});
-  },
+	},
 
-  delete: function(request, callback) {
+	delete: function(request, callback) {
 		this.connect((err, connection) => {
-      const currentId = request.body.source.id;
+			const currentId = request.body.source.id;
 			if (err) return callback(err)
-      r.db(config.db.name).table('currentWeather')
+			r.db(config.db.name).table('currentWeather')
 			.get(currentId)
 			.delete()
 			.run(connection)
@@ -67,7 +68,7 @@ module.exports = {
 				return callback(null, response);
 			})
 			.error((error) => {
-        logger.error(`Error trying to delete item in ${config.db.name}.`);
+				logger.error(`Error trying to delete item in the ${config.db.tables.currentWeather} table.`);
 				return callback(error);
 			});
 		});
