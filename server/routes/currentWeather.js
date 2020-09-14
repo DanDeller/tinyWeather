b321d7071c099d7fec5ae1f73e9f0c13d1be3c7a
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2020-09-14 17:48:15 -0400

    Server updates - removed models directory

--- server/routes/currentWeather.js
+++ server/routes/currentWeather.js
@@ -1,62 +1,51 @@
-const currentWeather = require('../models/currentWeatherModel'),
-      serverConfig = require('../../server.config.js'),
+const serverConfig = require('../../server.config'),
       MongoClient = require('mongodb').MongoClient,
       logger = require('../../middleware/logger'),
       express = require('express');
 
 const currentWeatherRoutes = express.Router();
 
-currentWeatherRoutes.get('/currentWeather', (req, res) => {
-  MongoClient.connect(serverConfig.mongo.connection, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }, (err, client) => {
-    const db = client.db(serverConfig.mongo.db);
-    const collection = db.collection(serverConfig.mongo.collections.currentWeather);
+MongoClient.connect(serverConfig.mongo.connection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(client => {
+  const db = client.db(serverConfig.mongo.db);
+  const collection = db.collection(serverConfig.mongo.collections.currentWeather);
 
+  currentWeatherRoutes.get('/currentWeather', (req, res) => {
     collection
     .find()
-    .toArray((err, items) => {
-      return res.send(items);
-    });
+    .toArray()
+    .then(result => {
+      return res.send(result);
+    })
+    .catch(err => console.log(err));
   });
-});
-
-currentWeatherRoutes.post('/currentWeather', (req, res) => {
-  MongoClient.connect(serverConfig.mongo.connection, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }, (err, client) => {
-    const db = client.db(serverConfig.mongo.db);
-    const collection = db.collection(serverConfig.mongo.collections.currentWeather);
+  
+  currentWeatherRoutes.post('/currentWeather', (req, res) => {
     const { city, id } = req.body;
 
     collection.insertOne({
       city: city, 
       id: id
-    }, (err, result) => {
-      if (err) console.log(err);
+    })
+    .then(result => {
       res.send(result);
-    });
+    })
+    .catch(err => console.log(err));
   });
-});
-
-currentWeatherRoutes.delete('/currentWeather', (req, res) => {
-  MongoClient.connect(serverConfig.mongo.connection, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }, (err, client) => {
-    const db = client.db(serverConfig.mongo.db);
-    const collection = db.collection(serverConfig.mongo.collections.currentWeather);
+  
+  currentWeatherRoutes.delete('/currentWeather', (req, res) => {
     const { id } = req.body;
 
     collection.deleteOne({
       id: id
-    }, (err, result) => {
-      if (err) console.log(err);
+    })
+    .then(result => {
       res.send(result);
-    });
+    })
+    .catch(err => console.log(err));
   });
-});
+}).catch(err => console.log(err));
 
 module.exports = currentWeatherRoutes;
