ad0027bed956b141666e56ef64308803977be9c4
deller26 <deller26@Daniels-MacBook-Pro.local>
2020-09-23 18:48:22 -0400

    FiveDayForecast updates

--- src/containers/FiveDayForecast/FiveDayForecast.js
+++ src/containers/FiveDayForecast/FiveDayForecast.js
@@ -7,13 +7,13 @@ import React, { useEffect } from 'react';
 import 'moment-timezone';
 
 const FiveDayForecast = () => {
-
+  const dispatch = useDispatch();
   const currentWeather = useSelector(state => state.currentWeather);
   const fiveDayForecast = useSelector(state => state.fiveDayForecast);
-  const dispatch = useDispatch();
+  const forgotCityInline = !currentWeather.setCity.length ? 'show' : 'hide';
   
   const getForecast = () => {
-    const fetchFlag = currentWeather.fetchFlag,
+    const fetchFlag = fiveDayForecast.fetchFlag,
           city      = currentWeather.setCity;
 
     if (currentWeather.setCity.length && !fetchFlag) {
@@ -25,9 +25,6 @@ const FiveDayForecast = () => {
   useEffect(() => {
     getForecast();
   }, []);
-
-  console.log(currentWeather)
-  const forgotCityInline = !currentWeather.setCity.length ? 'show' : 'hide';
 
   const days = fiveDayForecast.days.map((day, i) => (
     <div className={`day ${currentWeather.setCity.length === 0 ? 'hide' : 'show'}`} key={i}>
@@ -55,64 +52,6 @@ const FiveDayForecast = () => {
   );
 };
 
-// class FiveDayForecast extends React.Component {
-//   getForecast = () => {
-//     const fetchFlag = this.props.fetchFlag,
-//           city      = this.props.city;
-
-//     if (city.length && !fetchFlag) {
-//       this.props.dispatch(actions.fetchDays(city));
-//       this.props.dispatch(actions.setFetchFlag(!!this.props.fetchFlag));
-//     }
-//   };
-
-//   componentDidMount = () => {
-//     this.getForecast();
-//   };
-
-//   render() {
-//     const forgotCityInline = !this.props.city.length ? 'show' : 'hide';
-//     const days = this.props.days.map((day, i) => (
-//       <div className={`day ${this.props.city.length === 0 ? 'hide' : 'show'}`} key={i}>
-//         <p>
-//           <Moment format="dddd">
-//             {day.dt_txt.split(' ')[0]}
-//           </Moment>
-//           <span>{day.dt_txt.split(' ')[0]}</span>  
-//         </p>
-//         <p>{day.main.temp.toFixed(0)}</p>
-//         <p>{`${day.weather[0].description[0].toUpperCase()}${day.weather[0].description.slice(1)}`}</p>
-//       </div>
-//     ));
-
-//     return (
-//       <section className="container">
-//         <div className="weatherMain bodyText">
-//           <h1 className="pageHeader">Forecast for the next five days</h1>
-//           <div className="dayHold">
-//             <h3 className={`hide forgotCity ${forgotCityInline}`}>Go lookup a city first!</h3>
-//             {days}
-//           </div>
-//         </div>
-//       </section>
-//     );
-//   }
-// };
-
-const mapStateToProps = state => {
-  return {
-    fetchFlag: state.fiveDayForecast.fetchFlag,
-    city: state.currentWeather.setCity,
-    days: state.fiveDayForecast.days
-  }
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    dispatch
-  }
-};
-
 FiveDayForecast.propTypes = {
   fetchFlag: PropTypes.bool,
   dispatch: PropTypes.func,
@@ -120,7 +59,4 @@ FiveDayForecast.propTypes = {
   days: PropTypes.array
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(FiveDayForecast);
+export default FiveDayForecast;
