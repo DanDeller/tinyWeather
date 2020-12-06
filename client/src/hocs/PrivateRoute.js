2f7246dd996cd95f16f76b16991b18914d63702b
deller26 <deller26@Daniels-MacBook-Pro.local>
2020-12-05 23:45:46 -0500

    add fallback for suspense route

--- client/src/hocs/PrivateRoute.js
+++ client/src/hocs/PrivateRoute.js
@@ -10,7 +10,7 @@ const PrivateRoute = ({component: Component, ...rest}) => {
       if (!isAuthenticated) {
         return <Redirect to={{ pathname: '/about', state: { from: props.location }} }/>
       } else {
-        return <Suspense><Component {...props}/></Suspense>
+        return <Suspense fallback={<></>}><Component {...props}/></Suspense>
       };
     }}/>
   );
